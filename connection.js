85d36a5ef5e6841442e8154b7bf20f342585a184
NileshVS <50776400+NileshVS@users.noreply.github.com>
2019-09-29 19:15:59 +0530

    #fixed

--- connection.js
+++ connection.js
@@ -15,10 +15,24 @@ mongoose.connect('mongodb://localhost/mongo-assignment', { useNewUrlParser: true
 let userSchema = new mongoose.Schema({}, { strict: false }); //wihtout defining schema
 
 let userModel = mongoose.model('courses', userSchema);
-console.log(userModel.UserData);
-async function allUsers(){
-    let result = await userModel.find();
+// console.log(userModel.UserData);
+async function query1(){
+    let result = await userModel
+                        .find({tags:{$in:['backend']}, isPublished:true})
+                        .sort('name') 
+                        .select(['name', 'author']);
+    console.log('Query one:');
     console.log(result); 
 }
 
-allUsers();
+async function query2(){
+    let result = await userModel
+                        .find({isPublished: true})
+                        .sort('price')
+                        .select(['name', 'author']);
+                        console.log('Query two:');
+    console.log(result); 
+}
+
+query1();
+query2();
